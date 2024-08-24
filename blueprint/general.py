c3f4f8beb165efa0725a39f9719e401408667ec1
unknown <amirgorani466@gmail.com>
2024-08-24 12:54:40 +0330

    route (about) aded

---
+++ blueprint/general.py
@@ -0,0 +1,11 @@
+from flask import Blueprint
+app = Blueprint("general", __name__)
+
+@app.route('/')
+def main():
+    return 'this is main page'
+
+
+@app.route('/about')
+def about():
+    return 'about us'
